284cc5360e807fc305f561c0e7ab3c20091e20ee
Iatroph <joshuahilliard.hats@gmail.com>
2022-02-21 23:07:46 -0500

    Implemented Proper Turret Ai

--- Assets/_Scripts/TurretEnemy.cs
+++ Assets/_Scripts/TurretEnemy.cs
@@ -7,58 +7,106 @@ using UnityEngine;
 public class TurretEnemy : EnemyBase
 {
     public Transform Player;
-    public Transform turretBase;
-    public Transform turretBarrel;
+    public Transform turretHead;
+
+    [SerializeField] private float followSpeed = 3;
+    [SerializeField] private float range = 20;
+
+    [SerializeField] private float fireRate = 2;
+    [SerializeField] private float fireTimer;
+
+    private float distanceFromPlayer;
+
+    private float LockOnLength = 1.5f;
+    private float LockOnTimer;
+
+    [SerializeField] private GameObject Projectile;
+    [SerializeField] private Transform ProjectileSpawn;
+
 
     private void Start()
     {
-        
+        LockOnTimer = LockOnLength;
+        fireTimer = fireRate;
     }
 
     private void Update()
     {
-        //Vector3 dir = Player.position - transform.position;
-        //Quaternion look = Quaternion.LookRotation(dir);
-        //Vector3 rotation = Quaternion.Lerp(turretBase.rotation, look, 5 * Time.deltaTime).eulerAngles;
-        //turretBase.localRotation = Quaternion.Euler(0f, rotation.y, 0f);
+        distanceFromPlayer = Vector3.Distance(Player.position, turretHead.position);
+        Vector3 turretLookDir = Player.position - turretHead.position;
+        RaycastHit hit;
 
-        //Vector3 dir2 = Player.position - turretBarrel.position;
-        //Quaternion look2 = Quaternion.LookRotation(dir2);
-        //Vector3 rotation2 = Quaternion.Lerp(turretBarrel.rotation, look2, 5 * Time.deltaTime).eulerAngles;
-        ////turretBarrel.localRotation = Quaternion.Euler(turretBase.rotation.x, turretBase.rotation.y, rotation2.z);
-        //turretBarrel.localRotation = Quaternion.Euler(0, 0, rotation2.z);
+        //Debug.DrawLine(turretHead.position, turretHead.forward, Color.red);
+        Debug.DrawRay(turretHead.position, turretHead.forward * range, Color.red);
 
-        Test();
+        if (distanceFromPlayer <= 20)
+        {
+            if (Physics.Raycast(turretHead.position, turretLookDir, out hit, range))
+            {
+
+                if (hit.transform.CompareTag("Player"))
+                {
+                    Aim();
+                    Shoot();
+                    LockOnTimer = LockOnLength;
+                }
+
+
+                if (!hit.transform.CompareTag("Player"))
+                {
+                    LockOnTimer -= Time.deltaTime;
+                    if(LockOnTimer <= 0)
+                    {
+                        ReturnToNormal();
+                    }
+                    else
+                    {
+                        Shoot();
+                        Aim();
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            fireTimer = fireRate;
+            ReturnToNormal();
+        }
 
     }
 
-    void Aim(Vector3 targetPoint)
+    private void OnDrawGizmos()
     {
-
-
+        Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(turretHead.position, 20);
     }
 
-    private void Test()
+    private void Aim()
     {
-        //Vector3 turretLookDir = Player.position - transform.position;
-        ////Quaternion look = Quaternion.LookRotation(turretLookDir, transform.up);
-        //Quaternion look = XLookRotation(turretLookDir, transform.up);
-        //Vector3 rotation = Quaternion.Lerp(turretBase.localRotation, look, 5 * Time.deltaTime).eulerAngles;
-        ////Vector3 rotation = Quaternion.RotateTowards(turretBase.localRotation, look, 1).eulerAngles;
-        //turretBase.localRotation = Quaternion.Euler(transform.rotation.x, rotation.y, transform.rotation.z);
-
-        Vector3 turretLookDir = Player.position - transform.position;
-        //Quaternion look = Quaternion.LookRotation(turretLookDir, transform.up);
-        Quaternion look = XLookRotation(turretLookDir, transform.up);
-        //Vector3 rotation = Quaternion.Lerp(turretBase.localRotation, look, 5 * Time.deltaTime).eulerAngles;
-        Vector3 rotation = Quaternion.RotateTowards(turretBase.localRotation, look, 1).eulerAngles;
-
-        turretBase.localRotation = Quaternion.Euler(transform.rotation.x, rotation.y, transform.rotation.z);
-
-
-        //Vector3 flattenedVector = Vector3.ProjectOnPlane(-turretBase.right, Vector3.up);
-
+        Vector3 turretLookDir = Player.position - turretHead.position;
+        Quaternion look = Quaternion.LookRotation(turretLookDir, transform.up);
+        Vector3 rotation = Quaternion.Lerp(turretHead.rotation, look, followSpeed * Time.deltaTime).eulerAngles;
+        turretHead.rotation = Quaternion.Euler(rotation);
     }
+
+    private void Shoot()
+    {
+        fireTimer -= Time.deltaTime;
+
+        if(fireTimer <= 0)
+        {
+            GameObject projectile = Instantiate(Projectile, ProjectileSpawn.position, ProjectileSpawn.rotation);
+            fireTimer = fireRate;
+        }
+    }
+
+    private void ReturnToNormal()
+    {
+        Vector3 rotation = Quaternion.Lerp(turretHead.localRotation, Quaternion.Euler(0,0,0), 2 * Time.deltaTime).eulerAngles;
+        turretHead.localRotation = Quaternion.Euler(rotation);
+    }
+
 
     public Quaternion XLookRotation(Vector3 right, Vector3 up = default(Vector3))
     {
@@ -70,5 +118,6 @@ public class TurretEnemy : EnemyBase
 
         return forwardToTarget * rightToForward;
     }
+
 }
 
