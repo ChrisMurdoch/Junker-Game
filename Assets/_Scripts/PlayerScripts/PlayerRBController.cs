11872120ab2cd970f3f8f406f04acb7e4e1bcf56
Steven Mayo <steven.mayo16@gmail.com>
2022-07-24 22:13:50 -0400

    PlayerRB partially converted to state machine, approached adding walljumps

--- Assets/_Scripts/PlayerScripts/PlayerRBController.cs
+++ Assets/_Scripts/PlayerScripts/PlayerRBController.cs
@@ -52,13 +52,29 @@ public class PlayerRBController : MonoBehaviour
     /// </summary>
     public float speed;
     /// <summary>
-    /// speed for player to change direction
+    /// upward impulse added when player jumps
     /// </summary>
-    public float turnSpeed;
+    public float jumpImpulse;
+
     /// <summary>
-    /// upward force added when player jumps
+    /// The maximum number of times the player can jump in midair
     /// </summary>
-    public float jumpForce;
+    public float maxAirJumps = 1;
+
+    /// <summary>
+    /// The number of midair jumps the player currently has remaining
+    /// </summary>
+    private float airJumps;
+
+    /// <summary>
+    /// The time in seconds since the last jump the player did. Exists to avoid double jumping with one input
+    /// </summary>
+    private float jumpCooldownTimer = 0f;
+
+    /// <summary>
+    /// The time that must elapse after a jump before the player may jump again.
+    /// </summary>
+    private float jumpCooldownLength = 0.5f;
 
     //want momentum to build (only in air)
     //decelerate to ground speed when grounded
@@ -80,52 +96,135 @@ public class PlayerRBController : MonoBehaviour
     //momentum applied when jumping off wall
     //
 
+    /// <summary>
+    /// What the player is currently doing and how input should affect 
+    /// </summary>
+    public enum ActionState
+    {
+        /// <summary>
+        /// Player input does nothing. Use for cutscenes.
+        /// </summary>
+        Locked,
+        /// <summary>
+        /// Player is on the ground, and can walk, crouch, and jump
+        /// </summary>
+        Grounded,
+        /// <summary>
+        /// Player is in the air, and can influence their momentum and airjump
+        /// </summary>
+        Midair,
+        /// <summary>
+        /// Player is clinging to a wall, and can let go or walljump
+        /// </summary>
+        Wallcling
+    }
+
+    [HideInInspector]
+    public ActionState actionState = ActionState.Grounded;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         anim = gameObject.GetComponent<Animator>();
+        airJumps = maxAirJumps;
+        jumpCooldownTimer = 0f;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        horizontal = Input.GetAxis("Horizontal"); //get horizontal input
-
-        if((Input.GetAxis("Jump") > 0) && grounded) //check for jump input & grounded condition
+        Debug.Log("Jump Input: " + Input.GetAxis("Jump"));
+        switch(actionState)
         {
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //add force to initiate jump
-            anim.SetTrigger("jumping");
-            anim.SetBool("falling", true);
-            grounded = false;
+            case ActionState.Locked: break;
+            case ActionState.Grounded: Grounded(); break;
+            case ActionState.Midair: Midair(); break;
+            case ActionState.Wallcling: Wallcling(); break;
+            default: Debug.LogError("Uncaught Player Action State!"); break;
         }
+    }
+
+    /// <summary>
+    /// Set of code to be executed whenever player returns to the ground
+    /// </summary>
+    private void EnterGrounded()
+    {
+        actionState = ActionState.Grounded;
+        anim.SetBool("falling", false);
+        airJumps = maxAirJumps;
+        jumpCooldownTimer = 0f;
+    }
+
+    /// <summary>
+    /// Set of code to run on Update when the player is on the ground
+    /// </summary>
+    private void Grounded()
+    {
+        if (Input.GetAxis("Jump") > 0) //check for jump input
+        {
+            rb.AddForce(transform.up * jumpImpulse, ForceMode.Impulse); //add force to initiate jump
+            jumpCooldownTimer = jumpCooldownLength;
+            anim.SetBool("falling", true);
+            actionState = ActionState.Midair;
+        }
+
+        horizontal = Input.GetAxis("Horizontal"); //get horizontal input
 
         Vector3 velocity;
 
         if (facingForward) //imput stays normal while front facing
         {
-            velocity = (transform.forward * horizontal) * speed * Time.deltaTime;
-        } else //inputs flipped when facing backward
+            velocity = (transform.forward * horizontal) * speed;
+        }
+        else //inputs flipped when facing backward
         {
-            velocity = (-transform.forward * horizontal) * speed * Time.deltaTime;
+            velocity = (-transform.forward * horizontal) * speed;
         }
 
         velocity.y = rb.velocity.y; //precents gravity from being overridden by horizontal input
         rb.velocity = velocity; //apply velocity to rigidbody
 
         //apply current motion to animator
-        animatorMoveSpeed = velocity.x / speed / Time.deltaTime;
+        animatorMoveSpeed = velocity.x / speed;
         if (velocity.x == 0.0f)
             animatorMoveSpeed = 0.0f;
         if (!facingForward)
             animatorMoveSpeed *= -1.0f;
         anim.SetFloat("animatorMoveSpeed", animatorMoveSpeed);
 
-        Turn(); //check if you need to turn
+        Turn();
     }
 
-    private void Update()
+    private void Midair()
     {
+        
+        if (Input.GetAxis("Jump") > 0 && airJumps > 0) //check for jump input and remaining airjumps
+        {
+            if (jumpCooldownTimer < 0)
+            {
+                rb.AddForce(transform.up * jumpImpulse, ForceMode.Impulse); //add force to initiate jump
+                anim.Play("Falling Idle");
+                jumpCooldownTimer = jumpCooldownLength;
+                airJumps--;
+            }
+        }
+        jumpCooldownTimer -= Time.deltaTime;
+        Turn();
+    }
 
+    private void EnterWallcling()
+    {
+        Debug.Log("Entering Wallcling");
+        actionState = ActionState.Wallcling;
+        airJumps = maxAirJumps;
+        anim.SetBool("onWall", true);
+        anim.SetBool("falling", false);
+        rb.velocity = Vector3.zero;
+    }
+
+    private void Wallcling()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0.667f * rb.velocity.y, rb.velocity.z);
     }
 
     /// <summary>
@@ -157,28 +256,31 @@ public class PlayerRBController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Vector3 normal = collision.contacts[0].normal; //get the normal from one of the contact points
+        Vector3 normal = collision.GetContact(0).normal; //get the normal from one of the contact points
         float comparison = Vector3.Angle(normal, Vector3.up); //get an angle to see if the object can be ground
 
         Debug.Log("collision angle = " + comparison);
-        if(comparison <= 90f || comparison >= -90f) //make sure collision isn't with anything pointing slightly down
+        float acceptableGroundAngle = 75f;
+        if(comparison <= acceptableGroundAngle) //make sure collision isn't with anything pointing slightly down
         {
-            Debug.Log("COLLISION WITH GROUND");
-            if (grounded == false)
-                anim.SetTrigger("needsLanding");
-            grounded = true; 
+            if (actionState == ActionState.Midair)
+                EnterGrounded();
             ground = collision.gameObject; //keep track of the last ground you collided with
-            
-            anim.SetBool("falling", false);
+
+        }
+        else if(comparison > 85f && comparison < 95f && actionState == ActionState.Midair) //Check if a wall for a wallcling
+        {
+            Debug.Log("Collided with a wall");
+            EnterWallcling();
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (grounded && collision.gameObject == ground)
+        if (actionState == ActionState.Grounded && collision.gameObject == ground)
         {
             Debug.Log("exit collider");
-            grounded = false; //no longer grounded when you leave the ground you were on
+            actionState = ActionState.Midair; //no longer grounded when you leave the ground you were on
         }
     }
 }
